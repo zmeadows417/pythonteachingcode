a6c4a8b0eed0516fbbc1eab26078ae711640a4d0
zmeadows417 <zmeadows@students.kennesaw.edu>
2022-07-25 23:26:18 -0400

    Synth Project- Playing Chords on a Sine Wave ~~~
    Watched another couple YouTube tutorials and downloaded some more repositories. Need to incorporate the waves I was building as they sound a bit cleaner. Borrowed chords from a song called Borderline by Tame Impala, messed with the duration of the notes until it sounded nicer. Doesn't really sound anything like the song because of how much rhythm he adds to it. and the sound of the instrument is a lot cooler.

--- synth project/main.py
+++ synth project/main.py
@@ -1,7 +1,8 @@
-from msilib.schema import tables
-from this import d
 import numpy as np
+import pygame as py
 import scipy.io.wavfile as wav
+from scipy.io.wavfile import write
+
 
 def interpolate_linearly(wave_table, index) :
     truncated_index = int(np.floor((index)))
@@ -23,10 +24,10 @@ def fade_in_out(signal, fade_length=1000) :
     signal[-fade_length:] = np.multiply(fade_out, signal[-fade_length:])
 
     return signal
-
+sample_rate = 44100
 def a() :
     sample_rate = 44100
-    f = 440
+    f = 261.63 #c4 note
     t = 3
     waveform = sawtooth
 
@@ -52,6 +53,55 @@ def a() :
     amplitude = 10 ** (gain / 20)
     output *= amplitude
     
-    wav.write('sawtoothtest.wav', sample_rate, output.astype(np.float32))
+    wav.write('sawtooth.wav', sample_rate, output.astype(np.float32))
+
+def get_wave(freq, duration=2):
+    amplitude = 4096
+    t = np.linspace(0, duration, int(sample_rate * duration))
+    wave = amplitude * np.sin(2 * np.pi * freq * t)
+
+    return wave
+
+def get_piano_notes():
+    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
+    base_freq = 261.63
+
+    note_freqs = {octave[i]:base_freq*pow(2, (i/12)) for i in range(len(octave))}
+    note_freqs[''] = 0.0
+
+    return note_freqs
+
+def get_song_data(music_notes):
+    note_freqs = get_piano_notes()
+    song = [get_wave(note_freqs[note]) for note in music_notes.split('-')]
+    song = np.concatenate(song)
+
+    return song.astype(np.int16)
+
+
+def get_chord_data(chords):
+    chords = chords.split('-')
+    note_freqs = get_piano_notes()
+
+    chord_data = []
+    for chord in chords:
+        data = sum([get_wave(note_freqs[note]) for note in list(chord)])
+        chord_data.append(data)
+
+    chord_data = np.concatenate(chord_data, axis = 0)
+
+    return chord_data.astype(np.int16)
+
+if __name__ == '__main__':
+    music_notes = 'C-C-G-G-A-A-G--F-F-E-E-D-D-C--G-G-F-F-E-E-D--G-G-F-F-E-E-D--C-C-G-G-A-A-G--F-F-E-E-D-D-C'
+    data = get_song_data(music_notes)
+    data = data * (16300/np.max(data))
+
+    write('song.wav', sample_rate, data.astype(np.int16))
+
+    chords = 'CdG-GBD-aDF-FgC'
+    data = get_chord_data(chords)
+    data = data * (16300/np.max(data))
+    data = np.resize(data, (len(data)*5,))
     
-a()
+    write('borderline3.wav', sample_rate, data.astype(np.int16))
